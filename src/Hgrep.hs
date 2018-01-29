a2f8c0aa6810a546feccff37e2d2f9a9879df195
Fabian Meyer <user8324@posteo.net>
2018-01-29 09:50:47 +0100

    Add hgrep modules

---
+++ src/Hgrep.hs
@@ -0,0 +1,9 @@
+-- Hgrep.hs
+--
+--     Author: Fabian Meyer
+-- Created on: 29 Jan 2018
+
+module Hgrep (run) where
+
+run :: IO ()
+run = undefined
---
+++ src/Hgrep/Internal.hs
@@ -0,0 +1,9 @@
+-- Hgrep.Internal.hs
+--
+--     Author: Fabian Meyer
+-- Created on: 29 Jan 2018
+
+module Hgrep.Internal where
+
+run :: IO ()
+run = undefined
